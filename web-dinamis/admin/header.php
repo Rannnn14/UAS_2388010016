6e23629a5d1b045ba81c5a4064a64984599caa74
Faqih Firansyah <faqihfiransyah52@gmail.com>
2026-06-05 08:34:02 +0700

    update

--- web-dinamis/admin/header.php
+++ web-dinamis/admin/header.php
@@ -25,7 +25,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - E-Perpustakaan</title>
+    <title>Faqih Firansyah_2388010016</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <!-- FontAwesome untuk ikon gratis -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
